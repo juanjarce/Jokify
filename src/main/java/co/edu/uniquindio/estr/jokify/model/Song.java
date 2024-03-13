7bd7c6152d5e99e84d6d54f884944f9125e4f097
juanjarce <juanjtete2004@gmail.com>
2024-03-13 14:13:54 -0500

    User class structure added

--- src/main/java/co/edu/uniquindio/estr/jokify/model/Song.java
+++ src/main/java/co/edu/uniquindio/estr/jokify/model/Song.java
@@ -1,4 +1,8 @@
 package co.edu.uniquindio.estr.jokify.model;
 
-public class Song {
+public class Song implements Comparable<Song> {
+    @Override
+    public int compareTo(Song o) {
+        return 0;
+    }
 }
