b8e9e7e1975b455268e70eb4285f8c68d7eeb448
Joki <josedavidamayar@gmail.com>
2024-03-14 07:47:10 -0500

    Variables for the singIn controller

--- src/main/java/co/edu/uniquindio/estr/jokify/controllers/SingInController.java
+++ src/main/java/co/edu/uniquindio/estr/jokify/controllers/SingInController.java
@@ -1,8 +1,29 @@
 package co.edu.uniquindio.estr.jokify.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 
 public class SingInController {
+
+    @FXML
+    private TextField textFieldUser;
+
+    @FXML
+    private TextField textFieldPassword;
+
+    @FXML
+    private Button btnCreateAcount;
+
+    @FXML
+    private TextField textFieldEmail;
+
+    @FXML
+    private Hyperlink hpLogin;
+
     private Stage stage;
 
     public void setStage(Stage primaryStage) {
@@ -11,6 +32,16 @@ public class SingInController {
 
     public void show() {
         stage.show();
+    }
+
+    @FXML
+    void createAcount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
     }
 
 }
