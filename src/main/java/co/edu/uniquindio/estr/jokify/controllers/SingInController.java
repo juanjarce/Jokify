ea0cee34ab899a52d8b93ed4ba16309af11ab9be
Joki <josedavidamayar@gmail.com>
2024-03-14 07:35:56 -0500

    Creates controller for the SingIn interface

---
+++ src/main/java/co/edu/uniquindio/estr/jokify/controllers/SingInController.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.estr.jokify.controllers;
+
+import javafx.stage.Stage;
+
+public class SingInController {
+    private Stage stage;
+
+    public void setStage(Stage primaryStage) {
+        this.stage = primaryStage;
+    }
+
+    public void show() {
+        stage.show();
+    }
+
+}
