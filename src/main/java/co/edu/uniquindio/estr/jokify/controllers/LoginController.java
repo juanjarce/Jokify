55112431126dd25baeeb0dc91483d6e31c1c626d
Joki <josedavidamayar@gmail.com>
2024-03-14 07:44:14 -0500

    Variables for the login

--- src/main/java/co/edu/uniquindio/estr/jokify/controllers/LoginController.java
+++ src/main/java/co/edu/uniquindio/estr/jokify/controllers/LoginController.java
@@ -3,11 +3,24 @@ package co.edu.uniquindio.estr.jokify.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
+
+    @FXML
+    private TextField textFieldUser;
+
+    @FXML
+    private TextField textFieldPassword;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Hyperlink hpCreateAcount;
 
     private Stage stage;
 
@@ -17,6 +30,16 @@ public class LoginController {
 
     public void show() {
         stage.show();
+    }
+
+    @FXML
+    void createAcount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
     }
 
 }
