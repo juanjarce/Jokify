6ff80e6e259a5a8b20d5a12f5df115b0785f8b0f
Joki <josedavidamayar@gmail.com>
2024-03-21 22:55:45 -0500

    Methods in the controller to show the search

--- src/main/java/co/edu/uniquindio/estr/jokify/controllers/ShowSearchController.java
+++ src/main/java/co/edu/uniquindio/estr/jokify/controllers/ShowSearchController.java
@@ -4,6 +4,7 @@ import co.edu.uniquindio.estr.jokify.exceptions.SearchException;
 import co.edu.uniquindio.estr.jokify.model.Song;
 import co.edu.uniquindio.estr.jokify.model.Store;
 import co.edu.uniquindio.estr.jokify.model.User;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import java.net.URL;
@@ -11,13 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class ShowSearchController implements Initializable {
 
@@ -161,10 +165,69 @@ public class ShowSearchController implements Initializable {
             String search = txtSearch.getText();
             //Creates all the content that can be showed
             List<Song> songs = store.getSongsSearch(search);
-            System.out.println(songs.toString());
+            showSongs(songs);
         } catch (SearchException e) {
             showMessage("Jokify", "Busqueda", e.getMessage(), Alert.AlertType.WARNING);
         }
+    }
+
+    private void showSongs(List<Song> songs) {
+        if (songs.size() != 0) {
+            for (int i = 0; i < songs.size(); i++) {
+                if (i == 0 && songs.get(i) != null) {
+                    lblRS1.setText(songs.get(i).getName());
+                    Image aux = new Image(songs.get(i).getCover());
+                    imageRS1.setImage(aux);
+                    imageRS1.setFitWidth(200);
+                    imageRS1.setFitHeight(150);
+                    imageRS1.setTranslateY(10);
+                    animateFadeIn(imageRS1);
+                } else if (i == 1 && songs.get(i) != null) {
+                    lblRS2.setText(songs.get(i).getName());
+                    Image aux = new Image(songs.get(i).getCover());
+                    imageRS2.setImage(aux);
+                    imageRS2.setFitWidth(200);
+                    imageRS2.setFitHeight(150);
+                    imageRS2.setTranslateY(10);
+                    animateFadeIn(imageRS2);
+                } else if (i == 2 && songs.get(i) != null) {
+                    lblRS3.setText(songs.get(i).getName());
+                    Image aux = new Image(songs.get(i).getCover());
+                    imageRS3.setImage(aux);
+                    imageRS3.setFitWidth(200);
+                    imageRS3.setFitHeight(150);
+                    imageRS3.setTranslateY(10);
+                    animateFadeIn(imageRS3);
+                } else if (i == 3 && songs.get(i) != null) {
+                    lblRS4.setText(songs.get(i).getName());
+                    Image aux = new Image(songs.get(i).getCover());
+                    imageRS4.setImage(aux);
+                    imageRS4.setFitWidth(200);
+                    imageRS4.setFitHeight(150);
+                    imageRS4.setTranslateY(10);
+                    animateFadeIn(imageRS4);
+                } else if (i == 4 && songs.get(i) != null) {
+                    lblRS5.setText(songs.get(i).getName());
+                    Image aux = new Image(songs.get(i).getCover());
+                    imageRS5.setImage(aux);
+                    imageRS5.setFitWidth(200);
+                    imageRS5.setFitHeight(150);
+                    imageRS5.setTranslateY(10);
+                    animateFadeIn(imageRS5);
+                }
+            }
+        }
+    }
+
+    /**
+     * Applies a fade in animation to the specified node
+     * @param node
+     */
+    private void animateFadeIn(Node node) {
+        FadeTransition ft = new FadeTransition(Duration.millis(1000), node);
+        ft.setFromValue(0.0);
+        ft.setToValue(1.0);
+        ft.play();
     }
 
     @FXML
